f233eb1b30f68123efb5be7766b3aec842943434
BrunoFG1 <brunofarinhagoncalves@gmail.com>
2024-12-17 23:20:26 +0000

    Dataset class created

--- pred_skin.py
+++ pred_skin.py
@@ -3,6 +3,8 @@ import torchvision
 import os
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
 
 data = pd.read_csv('PH2_dataset.csv')
 
@@ -21,5 +23,31 @@ for fold, (test_set, train_set) in enumerate(skf.split(data, data['labels'])):
     train_data.to_csv(train_path, index=False)
     val_data.to_csv(val_path, index=False) 
 
+
+class CustomDataset(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+       
+        self.data = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        
+        img_name = self.data.iloc[index, 0]  
+        label = self.data.iloc[index, 1]    
+        
+        
+        img_path = os.path.join(self.img_dir, img_name)
+        image = Image.open(img_path)
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label 
+    
+     
 
 
